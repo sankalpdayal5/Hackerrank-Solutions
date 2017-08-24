a10dfbab1e6db36785679fa4145d2e04f8eef791
Sankalp Dayal <30744928+sankalpdayal5@users.noreply.github.com>
2017-08-24 21:57:28 +0530

    Add files via upload

---
+++ Intro to Conditional Statements.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=float(input())
+c=input()
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+print(i+a)
+# Print the sum of both integer variables on a new line.
+print(d+b)
+# Print the sum of the double variables on a new line.
+print(s+c)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
